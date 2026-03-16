e26993ed31e14aa09f1dfda809e82cfd267335eb
bhoomi1309 <bhoomitulsiyani@example.com>
2026-03-16 13:23:47 +0530

    Deployment ready

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaMariaDb({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    connectionLimit: 5
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
 });
 
 const prisma = new PrismaClient({ adapter });
